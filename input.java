7bb8da93de3ea02ce59706de2c0cebebafb39420
kowsik <138602726+kowsik22@users.noreply.github.com>
2025-03-13 14:59:35 +0530

    everything has been removed

--- input.java
+++ input.java
@@ -3,13 +3,7 @@ public class input {
     public static void main(String[] args) {
 
        
-       
-
-    // Method to sum two numbers
-    public static int sumTwoNumbers(int a, int b) {
-        return a + b;
     }
-    // Method to sum an array of integers
    
 
   
